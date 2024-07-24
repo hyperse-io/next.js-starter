b68fe9e82e960b8f46ba406024ac356af784bc1e
tianyingchun <tianyingchun@outlook.com>
2024-07-25 07:51:45 +0800

    fix: update `vitest.config.ts`

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,12 @@
-import { fileURLToPath } from 'url';
 import { configDefaults, defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    setupFiles: 'dotenv/config',
     globals: true,
     exclude: [...configDefaults.exclude, '**/playwright/**'],
     alias: {
-      '@/': fileURLToPath(new URL('./src/', import.meta.url)),
+      '@/': new URL('./src/', import.meta.url).pathname,
     },
-    passWithNoTests: true,
+    include: ['**/?(*.){test,spec}.?(c|m)[jt]s?(x)'],
   },
 });
